f5ddf1058c3ff20edd9086b1e2615734da10487f
Abdullah Ridwan <abdullahridwan73@gmail.com>
2023-01-12 22:37:59 -0500

    Running flutter run should make this work

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,13 @@ import 'package:halalapp/screens/onboarding_screen.dart';
 import 'screens/login_page.dart';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const HomeScreen());
 }
 
