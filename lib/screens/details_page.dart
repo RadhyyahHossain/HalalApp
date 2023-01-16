2d0de8a42b51adfa44c5b298de317f74150840a9
RadhyyahHossain <radiabxd@gmail.com>
2023-01-15 20:29:40 -0500

    added on details page

--- lib/screens/details_page.dart
+++ lib/screens/details_page.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: import_of_legacy_library_into_null_safe
+
 import 'package:flutter/material.dart';
 import 'package:halalapp/constants.dart';
 import 'package:halalapp/screens/home_page.dart';
+import 'package:smooth_star_rating/smooth_star_rating.dart';
 
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({super.key});
@@ -37,22 +40,70 @@ class DetailsScreen extends StatelessWidget {
               ),
               child: Column(
                 children: <Widget>[
+                  shopName(name: "Top Thai"),
                   Row(
                     children: <Widget>[
-                      Icon(
-                        Icons.location_on,
-                        color: Colors.grey[500],
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              "Steak and Fries",
+                              style: TextStyle(
+                                fontSize: 20,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            SmoothStarRating(
+                              borderColor: kPrimaryColor,
+                              rating: 4,
+                            ),
+                          ],
+                        ),
                       ),
-                      SizedBox(width: 10),
-                      Text("Top Thai"),
+                      Container(
+                        alignment: Alignment.center,
+                        padding: EdgeInsets.symmetric(vertical: 15),
+                        height: 65,
+                        width: 65,
+                        color: Colors.green,
+                        child: Text(
+                          "\$29",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
                     ],
                   ),
+                  Text(
+                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
                 ],
               ),
             ),
           ),
         ],
       ),
+    );
+  }
+
+  Row shopName({required String name}) {
+    return Row(
+      children: <Widget>[
+        Icon(
+          Icons.location_on,
+          color: Colors.grey[400],
+        ),
+        SizedBox(width: 10),
+        Text(
+          name,
+          style: TextStyle(
+            color: Colors.grey[600],
+          ),
+        ),
+      ],
     );
   }
 }
