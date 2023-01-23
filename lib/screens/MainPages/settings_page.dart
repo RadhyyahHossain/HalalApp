d17b6e76d6015e50b0266626fa80715b92caa8ba
Abdullah Ridwan <abdullahridwan73@gmail.com>
2023-01-22 23:15:27 -0500

    Changed settings page

--- lib/screens/MainPages/settings_page.dart
+++ lib/screens/MainPages/settings_page.dart
@@ -18,10 +18,54 @@ class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: _signOut,
-          child: const Text('sign out'),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(children: [
+            SettingsItem(
+              itemName: "Version: 1.0",
+              itemColor: Colors.black,
+              onTapMethod: () {},
+            ),
+            SettingsItem(
+                itemName: "Sign Out",
+                itemColor: Colors.red[400],
+                onTapMethod: _signOut),
+          ]),
+        ),
+      ),
+    );
+  }
+}
+
+class SettingsItem extends StatelessWidget {
+  SettingsItem(
+      {Key? key,
+      required this.itemName,
+      required this.itemColor,
+      required this.onTapMethod})
+      : super(key: key);
+  String itemName;
+  Color? itemColor;
+  void Function()? onTapMethod;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTapMethod,
+      child: Container(
+        width: double.maxFinite,
+        padding: EdgeInsets.all(12),
+        margin: EdgeInsets.symmetric(vertical: 7),
+        decoration: BoxDecoration(
+            color: Colors.grey.shade100,
+            borderRadius: BorderRadius.circular(10)),
+        child: Text(
+          itemName,
+          style: TextStyle(
+            fontWeight: FontWeight.w300,
+            color: itemColor,
+          ),
         ),
       ),
     );
